5cdfddcc14d15f7466d49ad12be64bf3a58a2d58
Yab <yabuman46@gmail.com>
2022-07-25 09:34:11 +0300

    Added a requierment.txt file

--- Hair_Project/Hair_Project/settings.py
+++ Hair_Project/Hair_Project/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_0bfw-y=915n6n5-xg3v5v=6i2%k3o-gao^vx*hiliwtpk2^4v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
